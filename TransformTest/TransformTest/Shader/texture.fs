2a171432819bd3b1c6cd7351b49b77167a662cbc
sharpwind612 <sharpwind612@gmail.com>
2019-08-19 01:07:54 +0800

    add glm library & init TransformTest project

---
+++ TransformTest/TransformTest/Shader/texture.fs
@@ -0,0 +1,12 @@
+#version 330 core
+out vec4 FragColor;
+in vec3 VertexColor;
+in vec2 TexCoord;
+uniform sampler2D texture1;
+uniform sampler2D texture2;
+uniform float mixValue;
+void main()
+{
+	FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), mixValue);
+//   FragColor = vec4(VertexColor,1.0f);
+}
